6bd9b8061431a1003334b86529ad7d123598b448
MatusPetrik211 <172436057+MatusPetrik211@users.noreply.github.com>
2024-12-30 18:15:49 +0100

    Make the code cleaner

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Function for setting the canvas size in blocks
 function createCanvas(size) {
     const container = document.querySelector('.container');
+    container.innerHTML = '';
 
     function createColumn() {
         const column = document.createElement('div');
@@ -23,23 +24,16 @@ function createCanvas(size) {
 
 function getSize() {
     const size = window.prompt('Enter the size of canvas: (maximum is 100)');
-    if(size > 100 || 0 > size || isNaN(size)) {
-        window.alert('Enter a valid size');
+    if(size < 100 && 0 < size) {
+        createCanvas(size); 
     }
     else {
-        const columns = document.querySelectorAll('.column');   
-        columns.forEach((column) => {
-            column.remove();
-        })
-        createCanvas(size); 
+        window.alert('Enter a valid size');
     }
 }
 
-function clear() {
-    const blocks = document.querySelectorAll('.block');
-    blocks.forEach((block) => {
-        block.setAttribute('style','background-color: white;');
-    });
+function clearCanvas() {
+    document.querySelectorAll('.block').forEach(block => block.setAttribute('style', 'background-color: white'));
 }
 
 function drawBlack() {
@@ -66,39 +60,20 @@ function drawRandom() {
     const blocks = document.querySelectorAll('.block');
     blocks.forEach((block) => {
         block.addEventListener('mouseover', () => {
-            const randomRed = Math.floor(Math.random() * 255);
-            const randomGreen = Math.floor(Math.random() * 255);
-            const randomBlue =  Math.floor(Math.random() * 255);
-            block.setAttribute('style',`background-color: rgb(${randomRed},${randomGreen},${randomBlue})`);
+            block.setAttribute('style',`background-color: rgb(${randomNumber()},${randomNumber()},${randomNumber()})`);
         });
     });
 }
 
+function randomNumber() {
+    return Math.floor(Math.random() * 255)
+}
+
+document.querySelector('.change-size').addEventListener('click', getSize);
+document.querySelector('.clear').addEventListener('click', clearCanvas);
+document.querySelector('.black').addEventListener('click', drawBlack);
+document.querySelector('.random').addEventListener('click', drawRandom);
+document.querySelector('.shading').addEventListener('click', drawShaded);
+
 createCanvas(16);
 drawBlack();
-
-const sizeButton = document.querySelector('.change-size');
-sizeButton.addEventListener('click', () => {
-    getSize();
-});
-
-const clearButton = document.querySelector('.clear');
-clearButton.addEventListener('click', () => {
-    clear();
-});
-
-const blackButton = document.querySelector('.black');
-blackButton.addEventListener('click', () => {
-    drawBlack();
-});
-
-const randomButton = document.querySelector('.random');
-randomButton.addEventListener('click', () => {
-    drawRandom();
-});
-
-const shadeButton = document.querySelector('.shading');
-shadeButton.addEventListener('click', () => {
-    drawShaded();
-})
-
